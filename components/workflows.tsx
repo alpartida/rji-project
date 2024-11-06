b14dc81d2091b6405605848b430b6a03a37414ff
Alex Partida <60553154+alpartida@users.noreply.github.com>
2024-11-06 15:29:32 -0800

    change the workflow images

--- components/workflows.tsx
+++ components/workflows.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import WorflowImg01 from "../public/images/workflow-01.png";
-import WorflowImg02 from "../public/images/workflow-02.png";
-import WorflowImg03 from "../public/images/workflow-03.png";
+import WorflowImg01 from "../public/images/wireframe.png";
+import WorflowImg02 from "../public/images/ab-testing.png";
+import WorflowImg03 from "../public/images/revenue-stock.png";
 import Spotlight from "../components/spotlight";
 import Link from "next/link";
 
